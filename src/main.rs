35fa914f00fd94c9485ce2b4726b031d56997ba6
Joe Kim <joesm.genie@gmail.com>
2026-01-06 12:07:52 +0700

    add configuration loading and error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use bookingapp::infrastructure::axum_http::http_serve::start;
+use bookingapp::{config::config_loader, infrastructure::axum_http::http_serve::start};
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
+
+    let dotenvy_config = match config_loader::load() {
+        Ok(config) => config,
+        Err(e) => {
+            error!("❌ Failed to load configuration: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("✅ Configuration loaded successfully: {:?}", dotenvy_config);
 
     start().await.expect("❌ Failed to start server!");
 }
