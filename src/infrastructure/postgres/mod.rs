45fc170d28962e08c02f4ec52d62bd0560065f64
Joe Kim <joesm.genie@gmail.com>
2026-01-16 17:43:34 +0700

    create database connection

--- src/infrastructure/postgres/mod.rs
+++ src/infrastructure/postgres/mod.rs
@@ -1,1 +1,2 @@
 pub mod schema;
+pub mod postgres_connection;
---
+++ src/infrastructure/postgres/postgres_connection.rs
@@ -0,0 +1,10 @@
+use anyhow::{Ok, Result};
+use diesel::{PgConnection, r2d2::{ConnectionManager, Pool}};
+
+pub type PgPoolSquad = Pool<ConnectionManager<PgConnection>>;
+
+pub fn establish_connection(database_url: &str) -> Result<PgPoolSquad> {
+  let manager = ConnectionManager::<PgConnection>::new(database_url);
+  let pool = Pool::builder().build(manager)?;
+  Ok(pool)
+}
