096763806c88801efa185b909a9cecf9b8c41469
Joe Kim <joesm.genie@gmail.com>
2026-01-16 17:45:27 +0700

    add repository modules for bookings, events, seats, zones, and users

--- src/infrastructure/postgres/mod.rs
+++ src/infrastructure/postgres/mod.rs
@@ -1,2 +1,3 @@
-pub mod schema;
 pub mod postgres_connection;
+pub mod repositories;
+pub mod schema;
---
+++ src/infrastructure/postgres/repositories/mod.rs
@@ -0,0 +1,6 @@
+pub mod booking_seats;
+pub mod bookings;
+pub mod events;
+pub mod seats;
+pub mod users;
+pub mod zones;
