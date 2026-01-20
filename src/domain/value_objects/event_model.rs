7eb67b69dff9ae4447153697ae1737214de2c4de
Joe Kim <joesm.genie@gmail.com>
2026-01-20 17:34:39 +0700

    create CreateEventModel for event data handling

---
+++ src/domain/value_objects/event_model.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use chrono::{DateTime, ParseError, Utc};
+use serde::{Deserialize, Serialize};
+
+use crate::domain::{
+    entities::events::CreateEventEntity, value_objects::event_statuses::EventStatuses,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateEventModel {
+    pub name: String,
+    pub description: String,
+    pub performer: String,
+    pub date: String,
+    pub location: String,
+}
+
+impl CreateEventModel {
+    pub fn to_entity(&self) -> Result<CreateEventEntity, ParseError> {
+        Ok(CreateEventEntity {
+            name: self.name.clone(),
+            description: self.description.clone(),
+            performer: self.performer.clone(),
+            date: DateTime::parse_from_rfc3339(&self.date)?
+                .with_timezone(&Utc)
+                .naive_utc(),
+            location: self.location.clone(),
+            status: EventStatuses::Scheduled.to_string(),
+            created_at: Utc::now().naive_utc(),
+            updated_at: Utc::now().naive_utc(),
+        })
+    }
+}
