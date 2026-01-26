dbdb40a30de301b4b0577c10504645e93ab0f2e9
Joe Kim <joesm.genie@gmail.com>
2026-01-26 10:26:27 +0700

    add unit test for create event use case

--- src/application/usecases/events.rs
+++ src/application/usecases/events.rs
@@ -1,6 +1,11 @@
 use std::sync::Arc;
 
-use crate::domain::repositories::events::EventsRepository;
+use anyhow::Result;
+use uuid::Uuid;
+
+use crate::domain::{
+    repositories::events::EventsRepository, value_objects::event_model::CreateEventModel,
+};
 
 pub struct EventsUseCase<T>
 where
@@ -15,5 +20,13 @@ where
 {
     pub fn new(events_repository: Arc<T>) -> Self {
         Self { events_repository }
+    }
+
+    pub async fn create(&self, create_event_model: CreateEventModel) -> Result<Uuid> {
+        let create_event_entity = create_event_model.to_entity()?;
+
+        let event_id = self.events_repository.create(create_event_entity).await?;
+
+        Ok(event_id)
     }
 }
---
+++ src/application/usecases/events_test.rs
@@ -0,0 +1,36 @@
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use anyhow::Ok;
+    use uuid::Uuid;
+
+    use crate::{
+        application::usecases::events::EventsUseCase,
+        domain::{repositories::events::MockEventsRepository, value_objects::event_model::CreateEventModel},
+    };
+
+    #[tokio::test]
+    async fn test_create_event() {
+        let mut mock_event_repo = MockEventsRepository::new();
+        let mock_event_id = Uuid::now_v7();
+        let mock_create_event_model = CreateEventModel {
+            name: "Test Event".to_string(),
+            description: "This is a test event.".to_string(),
+            performer: "Test Performer".to_string(),
+            date: "2026-02-01T10:00:00Z".to_string(),
+            location: "Test Location".to_string(),
+        };
+
+        mock_event_repo
+            .expect_create()
+            .returning(move |_| Box::pin(async move { Ok(mock_event_id) }));
+
+        let use_case = EventsUseCase::new(Arc::new(mock_event_repo));
+
+        let result = use_case.create(mock_create_event_model).await;
+
+        assert!(result.is_ok());
+        assert!(result.unwrap() == mock_event_id);
+    }
+}
